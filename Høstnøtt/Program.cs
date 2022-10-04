5e9b60a76d170cfc2c28831e85a23d8cc1a2d75b
aarda <aarda@DESKTOP-VHKJPCF>
2022-10-04 17:32:11 +0200

    Add project files.

---
+++ Høstnøtt/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Høstnøtt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10000; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (j == 0 || j == 2)
+                    {
+                        Console.WriteLine("________________");
+                        Console.WriteLine("|     ||||     |");
+                        Console.WriteLine("|     ||||     |");
+                        Console.WriteLine("|     ||||     |");
+                        Console.WriteLine("|     OOOO     |");
+                        Thread.Sleep(300);
+                        Console.Clear();
+                    }
+                    else if (j == 1)
+                    {
+                        Console.WriteLine("________________");
+                        Console.WriteLine("|     ||||     |");
+                        Console.WriteLine("|    / |||     |");
+                        Console.WriteLine("|   O  |||     |");
+                        Console.WriteLine("|      OOO     |");
+                        Thread.Sleep(300);
+                        Console.Clear();
+                    }
+                    else if (j == 3)
+                    {
+                        Console.WriteLine("________________");
+                        Console.WriteLine("|     ||||     |");
+                        Console.WriteLine("|     ||| \\    |");
+                        Console.WriteLine("|     |||  O   |");
+                        Console.WriteLine("|     OOO      |");
+                        Thread.Sleep(300);
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
